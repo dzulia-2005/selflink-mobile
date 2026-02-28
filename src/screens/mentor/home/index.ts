76f5cd8bb94bd6b0c0444938f7c70f7d247c1e39
georgetoloraia <gtoloraia19@gmail.com>
2026-02-28 21:50:24 +0400

    refactor(mentor): split mentor screens into submodules

---
+++ src/screens/mentor/home/index.ts
@@ -0,0 +1,1 @@
+export { MentorHomeScreen } from './view/MentorHomeScreen';
---
+++ src/screens/mentor/index.ts
@@ -0,0 +1,6 @@
+export { MentorHomeScreen } from './home';
+export { MentorChatScreen } from './chat';
+export { DailyMentorScreen } from './daily';
+export { DailyMentorEntryScreen } from './daily-entry';
+export { NatalMentorScreen } from './natal';
+export * from './routes';
